83a36dada434200de423585cad8c74be93df72c1
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-28 00:53:10 +0700

    chore(deploy): use docker compose

--- do_deploy.sh
+++ do_deploy.sh
@@ -6,15 +6,15 @@ StrictHostKeyChecking no
 EOF
   
 echo "stopping running application"
-echo ssh -i $DO_USER@$DO_IP 'docker stop ugmarket-web'
-ssh $DO_USER@$DO_IP 'docker rm ugmarket-web'
+sudo docker stop ugmarket-web
+sudo docker rm ugmarket-web
 
 echo "pulling latest version of the code"
-ssh -i $PATH_TO_PRIVATE_KEY $DO_USER@$DO_IP 'docker pull gust0/ugmarket:master'
-ssh -i $PATH_TO_PRIVATE_KEY $DO_USER@$DO_IP 'docker stop ugmarket-web'
-ssh -i $PATH_TO_PRIVATE_KEY $DO_USER@$DO_IP 'docker rm ugmarket-web'
+sudo docker pull gust0/ugmarket
+sudo docker stop ugmarket-web
+sudo docker rm ugmarket-web
 
 echo "starting the new version"
-ssh -i $PATH_TO_PRIVATE_KEY $DO_USER@$DO_IP 'docker run -d --restart=always --name ugmarket-web -p 80:4000 gust0/ugmarket:master'
+sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
 
 echo "success!"
