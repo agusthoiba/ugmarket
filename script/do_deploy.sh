322b5a8283cb77a4edda7d454a5add87caff1e4b
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-29 23:50:57 +0700

    chore(deploy): dodeploy - no need sudo

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -2,8 +2,11 @@
 # exit script when any command ran here returns with non-zero exit code
 set -e
 
+CONTAINER_NAME="ugmarket-web"
+
 echo "Starting Ugmarket Web"
 
-sudo docker run -d --restart=always --name ugmarket-web -p 80:4000 gust0/ugmarket
+docker stop $CONTAINER_NAME
+docker run -d --restart=always --name $CONTAINER_NAME -p 80:4000 gust0/ugmarket
 
 echo 'success'
