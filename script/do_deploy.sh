37dcb42790ef2b45d130e313286aabc11d691695
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-29 23:54:32 +0700

    chore(deploy): fix do_deploy - check no container existing running

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -6,7 +6,10 @@ CONTAINER_NAME="ugmarket-web"
 
 echo "Starting Ugmarket Web"
 
-docker stop $CONTAINER_NAME
+if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "true" ]; then
+    docker stop $CONTAINER_NAME
+fi
+
 docker run -d --restart=always --name $CONTAINER_NAME -p 80:4000 gust0/ugmarket
 
 echo 'success'
