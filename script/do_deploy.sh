385cb0c3428d72a5a2aa153784532d2ccc5daf33
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-30 00:03:06 +0700

    chore(deploy): fix dodeploy remove check dockername

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -6,10 +6,6 @@ CONTAINER_NAME="ugmarket-web"
 
 echo "Starting Ugmarket Web"
 
-if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "true" ]; then
-    docker stop $CONTAINER_NAME
-fi
-
 docker run -d --restart=always --name $CONTAINER_NAME -p 80:4000 gust0/ugmarket
 
 echo 'success'
