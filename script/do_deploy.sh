af5be0424598033054a49c9dec6c6f3bc18da751
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-29 23:14:12 +0700

    chore(deploy): again fix config ci and script to do

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -2,11 +2,7 @@
 # exit script when any command ran here returns with non-zero exit code
 set -e
 
-COMMIT_SHA1=$CIRCLE_SHA1
-
-# We must export it so it's available for envsubst
-export COMMIT_SHA1=$COMMIT_SHA1
-
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PWD
 sudo docker run -d --restart=always --name ugmarket-web -p 80:4000 gust0/ugmarket
 
 echo 'success'
