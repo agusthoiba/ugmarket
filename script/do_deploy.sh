e6c95b07e69eb353741c6c535c1d6e42550ce64c
Agus Thoiba <agus.thaiba@btpn.com>
2019-08-29 23:01:10 +0700

    chore(deploy): fix deploy ci

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -7,6 +7,6 @@ COMMIT_SHA1=$CIRCLE_SHA1
 # We must export it so it's available for envsubst
 export COMMIT_SHA1=$COMMIT_SHA1
 
-docker run -d --restart=always --name ugmarket-web -p 80:4000 gust0/ugmarket
+sudo docker run -d --restart=always --name ugmarket-web -p 80:4000 gust0/ugmarket
 
 echo 'success'
