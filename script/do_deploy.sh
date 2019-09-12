f1e3246f8551d59d8ae5cab4cbd6497d3995c50e
Agus Thoiba <agus.thaiba@btpn.com>
2019-09-13 01:53:59 +0700

    chore(ci): remove version for a while

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -8,8 +8,8 @@ IMAGE_NAMESPACE="gust0"
 echo "Starting Ugmarket Web"
 
 #Check for running container & stop it before starting a new one
-docker pull $IMAGE_NAMESPACE/$IMAGE_NAME:$BUILD_SHA1
+docker pull $IMAGE_NAMESPACE/$IMAGE_NAME
 docker rm $(docker stop $(docker ps -q -f name="$IMAGE_NAME"))
-docker run -d --restart=always --env-file=.env --name $CONTAINER_NAME -p 80:4000 $IMAGE_NAMESPACE/$IMAGE_NAME:$BUILD_SHA1
+docker run -d --restart=always --env-file=.env --name $CONTAINER_NAME -p 80:4000 $IMAGE_NAMESPACE/$IMAGE_NAME
 
 echo 'success'
