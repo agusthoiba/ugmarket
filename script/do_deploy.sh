38bea6cdd23e040d28f6ee906d42fb928cb5d85a
Agus Thoiba <agus.thaiba@btpn.com>
2019-09-13 02:04:28 +0700

    chore(deploy): fix deploy code

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -7,9 +7,8 @@ IMAGE_NAMESPACE="gust0"
 
 echo "Starting Ugmarket Web"
 
-#Check for running container & stop it before starting a new one
 docker pull $IMAGE_NAMESPACE/$IMAGE_NAME
 docker rm $(docker stop $(docker ps -q -f name="$IMAGE_NAME"))
-docker run -d --restart=always --env-file=.env --name $CONTAINER_NAME -p 80:4000 $IMAGE_NAMESPACE/$IMAGE_NAME
+docker run -d --restart=always --env-file=.env --name $IMAGE_NAME -p 80:4000 $IMAGE_NAMESPACE/$IMAGE_NAME
 
 echo 'success'
