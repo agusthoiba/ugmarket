62d5f5a7f3518ef5b24ee63915aaf61366aed40a
Agus Thoiba <agus.thaiba@btpn.com>
2019-09-13 01:50:01 +0700

    chore(): fix script do deploy

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -2,12 +2,14 @@
 # exit script when any command ran here returns with non-zero exit code
 set -e
 
-CONTAINER_NAME="ugmarket-web"
+IMAGE_NAME="ugmarket"
+IMAGE_NAMESPACE="gust0"
 
 echo "Starting Ugmarket Web"
 
 #Check for running container & stop it before starting a new one
-docker rm $(docker stop $(docker ps -q -f name="$CONTAINER_NAME"))
-docker run -d --restart=always --env_file=.env --name $CONTAINER_NAME -p 80:4000 gust0/ugmarket
+docker pull $IMAGE_NAMESPACE/$IMAGE_NAME:$BUILD_SHA1
+docker rm $(docker stop $(docker ps -q -f name="$IMAGE_NAME"))
+docker run -d --restart=always --env-file=.env --name $CONTAINER_NAME -p 80:4000 $IMAGE_NAMESPACE/$IMAGE_NAME:$BUILD_SHA1
 
 echo 'success'
