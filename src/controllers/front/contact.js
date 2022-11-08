e5c6b4e82647d29367f7a6c4a1b89860901e40f6
Gus Tho <agus.thaiba@btpn.com>
2022-11-08 23:10:08 +0700

    feature(contact): contact page ui n functionality

--- src/controllers/front/contact.js
+++ src/controllers/front/contact.js
@@ -3,11 +3,14 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     var obj = { 
         error: null, 
+        js: ['contact'],
         data: {
             breadcrumb: [
                 {link: '#', text: 'Kontak Kami'}
             ],
-            pageTitle: 'Kontak Kami'
+            pageTitle: 'Kontak Kami',
+            action: '/contact',
+            successPostMessage: null
         }
     };
 
@@ -25,11 +28,33 @@ router.post('/', async (req, res, next) => {
             breadcrumb: [
                 {link: '#', text: 'Kontak Kami'}
             ],
-            pageTitle: 'Kontak Kami'
-        }
+            pageTitle: 'Kontak Kami',
+            action: '/contact',
+            successPostMessage: 'Data anda berhasil tersimpan, akan kami respons segera'
+        },
+        
     };
-  
-  return res.render('front/contact', obj)
+
+    const payload = cleanPost(req.body);
+
+    try {
+        res.locals.contactModel.create(payload)
+        return res.render('front/contact', obj)
+    } catch (err) {
+        console.error('im error', err)
+        obj.error = err.message;
+        return res.render('front/contact', obj)
+    }  
 })
 
 module.exports = router
+
+function cleanPost(body) {
+    return {
+        ct_name: body.name.trim(),
+        ct_hp: body.hp.trim(),
+        ct_email: body.email.trim(),
+        ct_message: body.message.trim()
+    }
+}
+
