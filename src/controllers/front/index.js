71a129823428d79773c1e2d188afa7bc7537160d
Gus Tho <agus.thaiba@btpn.com>
2023-03-10 00:04:11 +0700

    feat(home): find raw model product

--- src/controllers/front/index.js
+++ src/controllers/front/index.js
@@ -2,9 +2,9 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   var obj = { error: null, data: { products: [], bands: [] } }
-  var query = { prod_is_visible: 1 }
-  var options = { sort: [['prod_id', 'DESC']], limit: 20 }
-  const doc = await res.locals.productModel.find(query, options)
+  var query = { prod_is_visible: 1, band_enabled: 1 };
+  const sort = {prod_id: 'DESC'};
+  const doc = await res.locals.productModel.findRaw(query, sort)
   
   if (doc.length > 0) {
     obj.data.products = doc.map(val => {
