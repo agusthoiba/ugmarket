ef2895b6de269167b997f59ef869997ecc9d95c6
gustho <agusthoiba@gmail.com>
2025-08-31 01:44:41 +0700

    chore(cicd): cicd circleci github

--- script/do_deploy.sh
+++ script/do_deploy.sh
@@ -1,4 +1,3 @@
-  GNU nano 4.8                                                                                do_deploy.sh
 #! /bin/bash
 # exit script when any command ran here returns with non-zero exit code
 set -e
@@ -6,21 +5,14 @@ set -e
 IMAGE_NAME="ugmarket"
 IMAGE_NAMESPACE="gust0"
 
-
 echo 'MYSQL_HOST: ' $MYSQL_HOST
 
 echo "Starting Ugmarket Web"
 
-sudo docker pull $IMAGE_NAMESPACE/$IMAGE_NAME
+docker pull $IMAGE_NAMESPACE/$IMAGE_NAME
+docker stop $IMAGE_NAME
+docker rm $IMAGE_NAME
 
-TAG_ID=$(sudo docker ps -q -f name="$IMAGE_NAME")
-echo 'TAG_ID: ' $TAG_ID
-
-if [ $TAG_ID !=  '' ]
-then
-   sudo docker rm $(sudo docker stop $(sudo docker ps -q -f name="$IMAGE_NAME"))
-fi
-
-sudo docker run -d --restart=always --env-file=.env --name $IMAGE_NAME -p 2000:4000 --add-host=host.docker.internal:$MYSQL_HOST $IMAGE_NAMESPACE/$IMAGE_NAME
+docker run -d --restart=always --env-file=.env --name $IMAGE_NAME -p 2000:4000 --add-host=host.docker.internal:$MYSQL_HOST $IMAGE_NAMESPACE/$IMAGE_NAME
 
 echo 'success'
